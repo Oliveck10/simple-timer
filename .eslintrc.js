fbceef3067028b912b07db186c9be50e23b9a0c7
Oliveck10 <oliveck10@gmail.com>
2016-12-05 05:10:35 +0800

    revise inital config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
   rules: {
       'no-console': 0,
       'no-else-return': 0,
+      'no-alert': 0,
       'new-cap': 0,
       'no-unused-vars': 0,
+      'prefer-template': 0,
+      'react/no-string-refs': 0,
   }
 };
