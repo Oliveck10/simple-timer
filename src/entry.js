b3b9a7763ef0a2127a1e034f429d825b8694644a
Oliveck10 <oliveck10@gmail.com>
2016-12-04 22:46:31 +0800

    add basic style config

---
+++ src/entry.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import SimpleTimer from './components/app';
+// import '../style/style.css';
+
+render(<SimpleTimer />, document.getElementById('timer'));
